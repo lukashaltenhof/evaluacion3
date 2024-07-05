5203a9dc97fcbadd4929414b45e5a1aa18bc745b
Beayemi <emiliopardocantillana@gmail.com>
2024-07-05 00:54:25 -0400

    carrito prototipo god

--- extremosur/gestionviajes/urls.py
+++ extremosur/gestionviajes/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('registro/', views.registro_cliente, name='registro_cliente'),
     path('agregar_paquete/', views.agregar_paquete, name='agregar_paquete'),
-    path('carrito/', views.ver_carrito, name='ver_carrito'),
+    path('ver-carrito/', views.ver_carrito, name='ver_carrito'),
     path('agregar_al_carrito/<int:paquete_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),
     path('pago/', views.pago, name='pago'),
     path('contacto/', views.contacto, name='contacto'),
@@ -24,5 +24,4 @@ urlpatterns = [
     path('contactos/create/', views.contacto_create, name='contacto_create'),
     path('contactos/update/<int:id>/', views.contacto_update, name='contacto_update'),
     path('contactos/delete/<int:id>/', views.contacto_delete, name='contacto_delete'),
-   
 ]
